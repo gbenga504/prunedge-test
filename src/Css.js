95b5e5d4c43e60e07c51f0f2568ce1f380cd76d7
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2019-08-24 21:49:13 +0100

    Add commonCss

---
+++ src/Css.js
@@ -0,0 +1,41 @@
+export const color = {
+  theme: "#0C66FF",
+  background: "#F3F5F7",
+  lightGrey: "#9E9E9E",
+  grey: "#999",
+  dark: "#43536F"
+};
+
+export const fontsize = {
+  small: 12,
+  base: 14,
+  medium: 16,
+  large: 18,
+  title: 18
+};
+
+export const fonts = {
+  main: '"Lato", "Helvetica Neue", sans-serif'
+};
+
+export const fontWeight = {
+  light: 300,
+  regular: 400,
+  bold: 700
+};
+
+export const commonCss = {
+  flex: {
+    alignItems: "center !important",
+    display: "flex !important",
+    flexShrink: 0
+  },
+  flexColumn: {
+    display: "flex !important",
+    flexDirection: "column"
+  },
+  flexGrow: {
+    display: "flex !important",
+    flexGrow: 1
+  }
+};
--- src/components/SideBar/styles.js
+++ src/components/SideBar/styles.js
@@ -1,30 +1,29 @@
-export const styles = theme => ({
+import { commonCss, fontsize, fonts, fontWeight, color } from "../../Css";
+
+export const styles = {
   container: {
+    ...commonCss.flexColumn,
     position: "absolute",
     left: 0,
     top: 0,
     height: "100%",
     width: 265,
     paddingBottom: 76,
-    display: "flex",
-    flexDirection: "column",
     justifyContent: "space-between",
     background: "#FFFFFF",
     boxShadow: "4px 0px 24px #E3E3E3"
   },
   topSectionContainer: {
-    display: "flex",
-    flexDirection: "column",
+    ...commonCss.flexColumn,
     marginTop: 95
   },
   userDetailsContainer: {
-    display: "flex",
-    flexDirection: "column",
+    ...commonCss.flexColumn,
     alignItems: "center"
   },
   avatarContainer: {
     borderRadius: 20,
-    border: "2px solid #7396FD",
+    border: `2px solid ${color.theme}`,
     background: "#fff",
     height: 85,
     width: 85,
@@ -38,32 +37,30 @@ export const styles = theme => ({
     }
   },
   userName: {
-    fontSize: 14,
-    fontFamily: "Lato",
-    color: "#5F5C7F",
-    fontWeight: 700,
+    fontSize: fontsize.base,
+    fontFamily: fonts.main,
+    color: color.dark,
+    fontWeight: fontWeight.bold,
     marginTop: 16
   },
   role: {
-    fontSize: 12,
-    color: "#999",
-    fontWeight: 400,
-    fontFamily: "Lato",
+    fontSize: fontsize.small,
+    color: color.lightGrey,
+    fontWeight: fontWeight.regular,
+    fontFamily: fonts.main,
     marginTop: 5
   },
   department: {
-    fontSize: 12,
-    color: "#999",
-    fontFamily: "Lato",
-    fontWeight: 400,
-    opacity: 0.7,
+    fontSize: fontsize.small,
+    color: color.lightGrey,
+    fontFamily: fonts.main,
+    fontWeight: fontWeight.regular,
     marginTop: 5
   },
   navItem: {
+    ...commonCss.flex,
     width: "100%",
     height: 48,
-    display: "flex",
-    alignItems: "center",
     paddingLeft: 40,
     cursor: "pointer"
   },
@@ -75,39 +72,38 @@ export const styles = theme => ({
       height: 48,
       left: 0,
       borderRadius: "0px 3px 3px 0px",
-      background: "#0C66FF"
+      background: color.theme
     }
   },
   icon: {
     fontSize: 26,
-    color: "#999",
+    color: color.lightGrey,
     marginRight: 10
   },
   iconActive: {
-    color: "#182C4F"
+    color: color.dark
   },
   navItemText: {
-    fontSize: 14,
-    fontFamily: "Lato",
-    color: "#999",
+    fontSize: fontsize.base,
+    fontFamily: fonts.main,
+    color: color.lightGrey,
     textTransform: "capitalize"
   },
   navItemTextActive: {
-    color: "#182C4F"
+    color: color.dark
   },
   badge: {
     right: -60,
     padding: "0px 7px",
-    backgroundColor: "#0C66FF"
+    backgroundColor: color.theme
   },
   logoutIcon: {
+    ...commonCss.flex,
     width: 26,
     height: 26,
-    background: "#999",
+    background: color.lightGrey,
     borderRadius: "50%",
-    display: "flex",
     marginRight: 10,
-    justifyContent: "center",
-    alignItems: "center"
+    justifyContent: "center"
   }
-});
+};
