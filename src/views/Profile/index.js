a79c26f7dadebca8e809a88e08fa8a024f39ee7b
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2019-08-24 17:31:19 +0100

    Add files

---
+++ src/views/Profile/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import withStyles from '@material-ui/core/styles/withStyles'
+
+import { styles } from './styles'
+
+class Profile extends React.PureComponent {
+    render() {
+        return null
+    }
+}
+
+export default withStyles(styles)(Profile)
